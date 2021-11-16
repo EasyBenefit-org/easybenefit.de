1e272cced6618bd32f3016168bac4622250369c8
Knivemare <35490770+Knivemare@users.noreply.github.com>
2021-11-16 13:50:57 +0100

    Prisma Integrated

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@
 import PageMetaTags from 'components/MetaTags/PageMetaTags';
 import Container from 'components/Container/Container';
 // Import Styles
-import PostCard from '../components/PostCard';
+import PostCard from '../components/PostCard'
+import prisma from 'lib/prisma';
 
-export default function Home({partners}) {
+export default function Home({partners, programms}) {
   return (
     <Container>
         <PageMetaTags
@@ -22,23 +23,45 @@ export default function Home({partners}) {
                 ))}
             </ul>
         )}
+
+        {programms.length === 0 ? (
+            <h2>No added partners</h2>
+        ) : (
+            <ul>
+                {programms.map((programm, i) => (
+                    <li key={i}>{programm.Name} - {new Date(programm.ActiveFrom).toLocaleDateString("de-DE", {day: "2-digit", month: "2-digit", year: "numeric"})}
+                    </li>
+                ))}
+            </ul>
+        )}
     </Container>
   )
 }
 
-export async function getServerSideProps(ctx) {
+export async function getStaticProps(ctx) {
     // get the current environment
-    let dev = process.env.NODE_ENV !== 'production';
-    let { DEV_URL, PROD_URL } = process.env;
+    let dev = process.env.NODE_ENV !== 'production'
+    let { DEV_URL, PROD_URL } = process.env
 
     // request posts from api
-    let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/partners`);
+    let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/partners`)
     // extract the data
-    let data = await response.json();
+    let data = await response.json()
+
+    // Prisma SQL lookup
+    const projects = await prisma.project.findMany({
+        include: {
+            DonationCategory: true
+        }
+    })
+    // Stringify and Pars to eliminate Date and Decimal serialization error
+    const projectsString = await JSON.stringify(projects)
+    const projectsJSON = await JSON.parse(projectsString)
 
     return {
         props: {
             partners: data['message'],
+            programms: projectsJSON
         },
     };
 }
